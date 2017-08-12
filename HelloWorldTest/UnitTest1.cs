0867e1b9561be4312e4980a7f9417b1f8e0d5fd2
Ralph Combe <sleepwalkinggeek@gmail.com>
2017-08-12 18:45:28 -0500

    Add files via upload
    Test

---
+++ HelloWorldTest/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace HelloWorldTest
+{
+    [TestClass]
+    public class HelloWorld
+    {
+        [TestMethod]
+        public static void PrintMessage()
+        {
+            Console.WriteLine("Hello World");
+            Console.ReadLine();
+        }
+    }
+}
